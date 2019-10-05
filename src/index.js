0f299a28fe29752c067d5ad32dec914e9ebcdf72
Овчиников Анатолий <aovchinnikov@MBP-Ovcinikov.Dlink>
2019-10-05 23:53:10 +0300

    Init commit, added base configs

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Hi!1111555</h1>, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dist'
+  }
+};
